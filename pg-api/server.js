18db1c7bdd80cff9a05e49e49fdcb89701e12284
Thomas Nicholson <nicholmen@gmail.com>
2018-01-21 18:56:47 -0800

    initial commit

---
+++ pg-api/server.js
@@ -0,0 +1,94 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let pg = require('pg');
+let cors = require('cors');
+const PORT = 3000;
+
+let pool = new pg.Pool({
+    port: 5432,
+    database: 'visited',
+    max: 10,
+    host: 'localhost',
+    user: 'thomasnicholson'
+});
+
+
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/countries', function (request, response) {
+  pool.connect(function(err, db, done) {
+    if(err) {
+      return response.status(400).send(err);
+    } 
+    else {
+      db.query('SELECT * FROM countries', function(err, table) {
+        done();
+        if(err) {
+          return response.status(400).send(err)
+        } 
+        else {
+          return response.status(200).send(table.rows)
+        }
+      })
+    }
+    
+  })
+})
+app.post('/api/new-country', function(request, response) {
+  var country_name = request.body.country_name;
+  var continent_name = request.body.continent_name;
+  var id = request.body.id;
+  let values = [country_name, continent_name, id]
+  pool.connect((err, db, done) => {
+    if(err) {
+      return response.status(400).send(err);
+    } else {
+      db.query('INSERT INTO countries (country_name, continent_name, id) VALUES ($1, $2, $3)', [...values], (err, table) => {
+        done();
+        if(err) {
+          return response.status(400).send(err)
+
+        } else {
+          console.log('DATA INSERTED');
+          response.status(201).send({message: 'Data inserted!'});
+        }
+      })
+    }
+  });
+})
+
+app.delete('/api/remove/:id', function(request, response) {
+  var id = request.params.id;
+  pool.connect(function(err, db, done) {
+    if(err) {
+      return response.status(400).send(err)
+    }
+    else {
+      db.query('DELETE FROM countries WHERE id = $1', [Number(id)], function(err, result){
+        done();
+        if(err) {
+          console.log(err)
+          return response.status(400).send(err)
+        }
+        else {
+          return response.status(200).send({message: 'success deleting record'})
+        }
+      })
+    }
+  })
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
---
+++ visited-react/src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      title: ' Visited country application',
+      countries: []
+    }
+  }
+  
+
+  // make ajax calls here
+
+  componentDidMount() {
+    console.log('Component has mounted')
+    var that = this;
+    fetch('http://localhost:3000/api/countries')
+      .then(function(response){
+        response.json()
+          .then(function(data) {
+          let countries = that.state.countries;
+          countries.concat(data); 
+          that.setState({
+            countries: data
+        })
+      })
+  })
+}
+
+removeCountry(id) {
+  var that = this;
+  let countries = this.state.countries;
+  let country = countries.find(function(country){
+    return country.id === id
+  });
+
+  var request = new Request('http://localhost:3000/api/remove/' + id, {
+    method: 'DELETE'
+  });
+
+  fetch(request)
+  .then(function(response) {
+    countries.splice(countries.indexOf(country), 1);
+    that.setState({
+      countries: countries
+    })
+    response.json()
+    .then(function(data) {
+      console.log(this)
+    })
+  })
+}
+
+  addCountry(event) {
+    var that = this
+    event.preventDefault(); // prevent form from submitting
+    let country_data = {
+      country_name: this.refs.country_name.value,
+      continent_name: this.refs.continent_name.value,
+      id: Math.random().toFixed(3)
+    };
+    var request = new Request('http://localhost:3000/api/new-country', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json'}),
+      body: JSON.stringify(country_data)
+    });
+
+    let countries = that.state.countries;
+    countries.push(country_data);
+    that.setState({
+      countries: countries
+    })
+    fetch(request)
+      .then(function(response) {
+        response.json()
+          .then(function(data) {
+           
+        })
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+  }
+
+  render() {
+    let title = this.state.title;
+    let countries = this.state.countries;
+    return (
+      <div className="App">
+        <h1>{title}</h1>
+        <form ref="countryForm">
+          <input type="text" ref="country_name" placeholder="country name"/>
+          <input type="text" ref="continent_name" placeholder="continent name"/>
+          <button onClick={this.addCountry.bind(this)}>Add country</button>
+        </form>
+        <ul>
+          {countries.map(country => <li key={country.id}>{country.country_name} {country.continent_name} <button onClick={this.removeCountry.bind(this, country.id)}>Remove</button> </li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
